b262d3c9c11f0d58532c01bb43bd76edd61c290c
tsbaycheva <ccveti@abv.bg>
2016-08-16 15:44:55 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR_System;
+
+namespace HR_System
+{
+    public class Employee 
+    {
+        public static void Main()
+        {
+            //make an employee list
+            IDictionary<string, string> Employees =
+                new Dictionary<string, string>();
+
+            EmployeeData.ExistingEmployees(Employees);
+
+            // The customer has to make a choice - if he wants to add an employee either search an existing one
+            Console.WriteLine("Dear Customer, \nIf you want to see the information for the existing employee, \nplease type 'Search'.\nIf you want to add some information, please type 'Add'");
+            string userChoice = Console.ReadLine();
+
+                if (userChoice == "Add")
+                {
+                    EmployeeData.InsertEmployeeData();
+                }
+
+                else if (userChoice == "Search")
+                {
+                    //enter name for the existing employee
+                    Console.Write("\nPlease enter name of the employee: ");
+                    string existingEmp = Console.ReadLine();
+                        if (Employees.ContainsKey(existingEmp))
+                        {
+                            EmployeeData.SearchEmployee(Employees, existingEmp);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nThere is no employee named " + existingEmp);
+                            Console.WriteLine("\nIf you want to see the list with all the employees, please enter 'List'");
+
+                            string list = Console.ReadLine();
+                                if (list == "List")
+                                {
+                                    EmployeeData.ShowExistingEmployees(Employees);
+
+                                    //Delete an existing employee from the list
+                                    Console.WriteLine("\nIf you want to delete an employee, please enter 'Delete'");
+                                    string delete = Console.ReadLine();
+
+                                    if (delete == "Delete")
+                                    {
+                                        Console.Write("Please enter name: ");
+                                        string deleteName = Console.ReadLine();
+                                        Employees.Remove(deleteName);
+                                        Console.WriteLine(deleteName + " has been removed!");
+
+                                        Console.WriteLine("If you want to see the employeelist, please enter 'List'");
+                                        string deletelist = Console.ReadLine();
+                                            if (deletelist == "List")
+                                            {
+                                                EmployeeData.ShowExistingEmployees(Employees);
+                                            }
+                                    }
+                        }
+                    }
+            }
+        }
+    }
+
+}
---
+++ EmployeeData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System
+{
+    class EmployeeData
+    {
+        public string empName 
+        {
+            get { return this.empName; }
+            set { this.empName = value; }
+        }
+        public string empPosition
+        {
+            get { return this.empPosition; }
+            set { this.empPosition = value; }
+        }
+        public string empPhone
+        {
+            get { return this.empPhone; }
+            set { this.empPhone = value; }
+        }
+        public string empMail
+        {
+            get { return this.empMail; }
+            set { this.empMail = value; }
+        }
+
+        public static void ExistingEmployees(IDictionary<string, string> Employees)
+        {
+            Employees["Asen Asenov"] = "\nID - 4545 , \nPosition - Trainee, \ne-mail - a.asenov@test.com , \nPhone - 0889/455620";
+            Employees["Maria Marinova"] = "\nID - 4546, \nPosition - Junior, \ne-mail - m.marinovav@test.com , \nPhone - 0889/456203";
+            Employees["Ivan Ivanov"] = "\nID - 4547, \nPosition - Senior, \ne-mail - i.ivanov@test.com , \nPhone - 0889/789526";
+            Employees["Simona Simeonova"] = "\nID - 4548, \nPosition - Intermediate, \ne-mail - s.simeonova@test.com , \nPhone - 0889/203563";
+            Employees["Georgi Georgiev"] = "\nID - 4549, \nPosition - Team leader, \ne-mail - g.georgiev@test.com , \nPhone - 0889/015697";
+            Employees["Viktoria Viktorova"] = "\nID - 4550, \nPosition - Project manager, \ne-mail - v.viktorova@test.com , \nPhone - 0889/748523";
+            Employees["Mila Markova"] = "\nID - 4551 , \nPosition - CEO, \ne-mail - m.markova@test.com , \nPhone - 0889/852145";
+        }
+        public static void InsertEmployeeData()
+        {
+            // insert data for the employee
+            Console.Write("\nPlease enter name: ");
+            string empName = Console.ReadLine();
+            Console.Write("Please enter position: ");
+            string empPosition = Console.ReadLine();
+            Console.Write("Please enter phone: ");
+            string empPhone = Console.ReadLine();
+            Console.Write("Please enter e-mail: ");
+            string empMail = Console.ReadLine();
+
+            //print the information that the cutomer just entered
+            Console.WriteLine("\nHere is the information you just entered: ");
+            Console.WriteLine("Name: " + empName + "\nPosition: " + empPosition + "\nPhone: " + empPhone + "\ne-mail: " + empMail);
+        }
+
+        public static void SearchEmployee(IDictionary<string, string> Employees, string existingEmp)
+        {
+            foreach (var item in Employees)
+            {
+                if (item.Key.ToString() == existingEmp)
+                {
+                    Console.WriteLine("\nThe result for  " + item.Key + " is: \n" + item.Value);
+                }
+            }
+        }
+
+        public static void ShowExistingEmployees(IDictionary<string, string> Employees)
+        {
+            Console.WriteLine("\nThere are " + Employees.Count + " employees in the list:");
+
+            foreach (KeyValuePair<string, string> pair in Employees)
+            {
+                Console.WriteLine("\nName = " + pair.Key + pair.Value);
+            }
+        }
+    }
+}
