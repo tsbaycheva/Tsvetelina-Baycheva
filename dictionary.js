5def268f5b6efac0bff2f2445487f40174216169
tsbaycheva <ccveti@abv.bg>
2016-10-03 00:53:32 +0300

    Add files via upload

--- dictionary.js
+++ dictionary.js
@@ -3,30 +3,71 @@
 
     angular
         .module('app')
-        .factory('dictionary', service);
+        .directive('dictionary', dictionary)
+        .config(config);
 
-    service.$inject = ['$dictionary'];
+    config.$inject = ['$stateProvider'];
+    function config($stateProvider) {
+        $stateProvider
+            .state('dictionary', {
+                url: '/dictionary',
+                template: '<dictionary ></dictionary>',
+                params: {site: ''}
+            });
+    }
 
-    function service($dictionary) {
+    function dictionary() {
+        var directive = {
+            templateUrl: './states/dictionary/dictionary.html',
+            restrict: 'E',
+            controller: controller,
+            scope: {}
+        };
 
-        var table = {
-            {
+        return directive;
+    }
+
+    controller.$inject = ['$scope'];
+    function controller($scope) {
+        $scope.table =[
+        {
             'name' : 'kljenj',
             'description' : 'kvbejiobneriovbrnvbier'
-             },
-             {
+        },
+        {
             'name' : 'njedogjer',
             'description': 'klnbbbolmoebenrobvrnme'
-            },
-            {
+        },
+        {
             'name' : 'nejvne',
             'description': 'lkvnmoblerkn'
-            },
+        },
+        ]; 
+
+        $scope.addRow = function() {
+            $scope.table.push({'name':$scope.name, 'description' : $scope.description});
+            $scope.name = '';
+            $scope.description = '';
         }
 
-        return {
-           getDictionary: table
+        $scope.editExercises = function(x) {
+            $scope.current = angular.copy(x);
+            x.editMode = true;
         };
+
+         $scope.saveChanges = function(x) {
+                x.editMode = false;
+            };
+
+        $scope.removeExercise = function(index) {
+            $scope.table.splice(index, 1);
+        };
+
+        $scope.cancel = function(x) {
+            x.editMode = false;
+            x.name = $scope.current.name;
+            x.description = $scope.current.description;
+        }
     }
 
 }(angular));
